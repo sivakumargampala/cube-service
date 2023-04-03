1c12c9d354567e0ee73abbd978e4e934ef6bf243
Siva Kumar Gampala <sivakumarg2009@gmail.com>
2023-04-03 15:18:42 +0530

    Update CubeServiceController.java
    cross origin

--- src/main/java/com/poc/cube/CubeServiceController.java
+++ src/main/java/com/poc/cube/CubeServiceController.java
@@ -1,10 +1,12 @@
 package com.poc.cube;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class CubeServiceController {
 	
 	@GetMapping("/cube/{inputNum}")
